1bbad73beb4bf16616fa1857aff8bf5f64b84ca5
Fux <109365408+Fuxfantx@users.noreply.github.com>
2024-01-30 04:28:41 +0800

    20240130  #1

--- Python/Prototypia/Arf2.py
+++ Python/Prototypia/Arf2.py
@@ -17,12 +17,14 @@ from . import WishGroup as WishGroupFb
 
 # Error Handling & Tools
 E = Exception
+class BPMInvalidError			(E):pass
+class ScaleInvalidError			(E):pass
 class ManualProhibition			(E):pass
-class PnOverlapProhibition		(E):pass
+class OverlapProhibition		(E):pass
 
-def NonManual(f):
+def NonManual(f) -> FunctionType:
 	def decorated(*args, **kwargs) -> None:
-		if len( inspect.stack() ) < 3 : raise ManualProhibition('''The "new" method is prohibited to call manually.''')
+		if inspect.stack()[1].function == "<module>" : raise ManualProhibition('''This method is prohibited to call manually.''')
 		return f(*args, **kwargs)
 	return decorated
 
@@ -31,25 +33,31 @@ def PosNodeSorter(a,b) -> int:
 	if a.bartime > b.bartime: return 1
 	elif a.bartime < b.bartime: return -1
 	elif id(a) == id(b): return 0
-	else: raise PnOverlapProhibition('''Adding multiple PosNodes with the same Bartime to a single WishGroup is prohibited.''')
+	else: raise OverlapProhibition('''Adding multiple PosNodes with the same Bartime to a single WishGroup is prohibited.''')
 
 
-# Arf2 Data Structure Classes
+# Arf2 Prototype
 class Arf2Prototype:
 	'''
 	A rough Singeleton class of the Prototype Data of an Arf2 chart[fumen].
 	'''
-	_hint:list				= []	# Type: list[Hint]
-	_wish:list				= []	# Type: list[WishGroup]
-	_offset:int				= 0		# A ms value (>=0)
-	_current_angle:int		= 90	# Degree [-1800,1800]
-	_last_hint				= None	# Type: Hint
-	_bpms:list				= []	# (Bartime, BPM)
-	_scales_layer1:list		= []	# (Bartime, Scale)
-	_scales_layer2:list		= []	# Bartime>=0   BPM>0
-	_wgo_required:int		= 255   # [0,255]
-	_hgo_required:int		= 255   # [0,255]
-	
+	_wgo_required:int							= 255   		# [0,255]
+	_hgo_required:int							= 255   		# [0,255]
+	_hint:list									= []			# Type: list[Hint]
+	_wish:list									= []			# Type: list[WishGroup]
+
+	_offset:int									= 0				# A ms value (>=0)
+	_bpms:list[Tuple[float,float]]				= []			# (Bartime, BPM)
+	_scales_layer1:list[Tuple[float,float]]		= [(0,1)]		# (Bartime, Scale)
+	_scales_layer2:list[Tuple[float,float]]		= [(0,1)]		# Bartime>=0   BPM>0
+
+	_current_angle:int							= 90			# Degree [-1800,1800]
+	_last_hint									= None			# Type: Hint
+	_sc1_dflt:bool								= True
+	_sc2_dflt:bool								= True
+
+
+# Arf2 Data Structure Classes
 class Hint:
 	'''
 	Hint represents the Tap Note of Aerials.
@@ -111,13 +119,13 @@ class WishGroup:
 	'''
 	@NonManual
 	def __init__(self, ofLayer2:bool = False, MaxVisibleDistance:float = 7) -> None:
-		self.nodes:list[PosNode] = []
-		self.childs:list[WishChild] = []
-		self.of_layer2 = ofLayer2
-		self.max_visible_distance = MaxVisibleDistance
-		self._last_child:WishChild = None
-		self._useless:bool = False
-	
+		self.__nodes:list[PosNode] = []
+		self.__childs:list[WishChild] = []
+		self.__of_layer2 = ofLayer2
+		self.__max_visible_distance = MaxVisibleDistance
+		self.__last_child:WishChild = None
+		self.__useless:bool = False
+
 	# Several Methods for the WishGroup instance
 	def n(self, bar:float, nmr:int=0, dnm:int=1, x:float=0, y:float=0, easetype:int=0, curve_init:float=0, curve_end:float=1) -> Self:
 		'''
@@ -133,7 +141,7 @@ class WishGroup:
 			easetype (int): EaseType of the AngleNode. Use Pn.* Series.
 			curve_init (float): The initial ratio of the easing curve. Range[0,1]
 			curve_end (float): The end ratio of the easing curve. Range[0,1]
-		
+
 		Returns:
 			Self (WishGroup): for Method Chaining Usage.
 		'''
@@ -152,10 +160,10 @@ class WishGroup:
 			raise ValueError("CurveEnd out of Range [0,1].")
 		if curve_init >= curve_end:
 			raise ValueError("CurveEnd Value must be larger than CurveInit Value.")
-		
+
 		n = PosNode(bartime, x, y, easetype, curve_init, curve_end)
-		self.nodes.append(n)
-		self.nodes.sort(key = PosNodeSorter)
+		self.__nodes.append(n)
+		self.__nodes.sort(key = PosNodeSorter)
 		return self
 
 	def c(self, bar:float, nmr:int=0, dnm:int=1, angle:int=Arf2Prototype._current_angle) -> Self:
@@ -175,7 +183,7 @@ class WishGroup:
 			dnm (int): denominator to specify the internal position in a bar. Example 5/16 -> 16
 			degree (int): The polar coordinate angle value in degree form.
 			easetype (int): EaseType of the AngleNode. Use An.* Series.
-		
+
 		Returns:
 			Self (WishGroup): for Method Chaining Usage.
 		'''
@@ -186,10 +194,10 @@ class WishGroup:
 			raise ValueError("Degree of AngleNode out of Range [-1800,1800].")
 		if easetype not in [0,1,2,3]:
 			raise TypeError("Invalid AngleNode EaseType. Use An.* Series.")
-		if self._last_child == None:
+		if self.__last_child == None:
 			raise RuntimeError("Please attach at least one WishChild on the WishGroup before creating a AngleNode.")
 
-		_lc = self._last_child
+		_lc = self.__last_child
 		if _lc.is_default_angle:
 			_lc.anodes[0] = (bartime,degree,easetype)
 			_lc.is_default_angle = False
@@ -206,19 +214,19 @@ class WishGroup:
 			bar (float): Bartime integer or the Original Bartime
 			nmr (int): numerator to specify the internal position in a bar. Example 5/16 -> 5
 			dnm (int): denominator to specify the internal position in a bar. Example 5/16 -> 16
-		
+
 		Returns:
 			Self (WishGroup): for Method Chaining Usage.
 		'''
 		bartime = bar+nmr/float(dnm)
 		if bartime<0:
 			raise ValueError("Bartime(bar+nmr/dnm) must be larger than 0.")
-		
+
 		h = Hint(bartime, self)
 		Arf2Prototype._hint.append(h)
 		Arf2Prototype._last_hint = h
 		return self
-	
+
 	def set_useless(self, useless:bool) -> Self:
 		'''
 		Wishes tagged as "useless" will be ignored in the compiling process,
@@ -227,12 +235,23 @@ class WishGroup:
 
 		Args:
 			useless (bool): Should the calling WishGroup instance be tagged as "useless"?
-		
+
 		Returns:
 			Self (WishGroup): for Method Chaining Usage.
 		'''
-		self._useless = useless
+		self.__useless = useless
 		return self
+	
+	
+	@NonManual
+	def _EXPORT_(self) -> dict: return {
+		"nodes" : self.__nodes,
+		"childs" : self.__childs,
+		"of_layer2" : self.__of_layer2,
+		"max_visible_distance" : self.__max_visible_distance,
+		"last_child" : self.__last_child,
+		"useless" : self.__useless
+	}
 
 
 # Arf2 Compiler Function
--- Python/Prototypia/__init__.py
+++ Python/Prototypia/__init__.py
@@ -1,62 +1,312 @@
+'''
+Methods Provided:   # See also  GitHub.com/1dealGas/Prototypia  . 
+    "An", "Pn", "REQUIRE",
+    "OFFSET", "BEATS_PER_MINUTE", "BARS_PER_MINUTE", "SC_LAYER1", "SC_LAYER2",
+    "CURRENT_ANGLE", "specialize_last_hint", "w", "wn1", "wn2",
+
+'''
+
 from .Arf2 import *
 __all__ = [
-	"An", "Pn",
+    "An", "Pn", "REQUIRE",
     "OFFSET", "BEATS_PER_MINUTE", "BARS_PER_MINUTE", "SC_LAYER1", "SC_LAYER2",
-    "CURRENT_ANGLE", "specialize_last_hint", "w",
+    "CURRENT_ANGLE", "specialize_last_hint", "w", "wn1", "wn2",
 
-	"REQUIRE"
 ]
 
 
-class An:   # EaseType Enums
+# Functional Classes
+class An:
+	'''EaseType Enums for AngleNodes.'''
 	STASIS = 0
 	LINEAR = 1
 	ESIN = 2
 	ECOS = 3
-class Pn:   # EaseType Enums
+class Pn:
+	'''EaseType Enums for PosNodes.'''
 	LINEAR = 0
 	LCIRC = 1
 	RCIRC = 2
 	INQUAD = 3
 	OUTQUAD = 4
-class _R:   # Managing wgo_required & hgo_required
-	cnt = 0
+class __R:
+	'''An internal class managing wgo_required & hgo_required.'''
+	__cnt = 0
 	def __getitem__(self, idx):
-		if _R.cnt == 0:
-			Arf2Prototype._wgo_required = idx
-			_R.cnt = 1
-		elif _R.cnt == 1:
-			Arf2Prototype._hgo_required = idx
-			_R.cnt = 2
+		if __R.__cnt == 0:
+			__idx = int(idx)
+			if __idx < 0 or __idx > 255: raise ValueError("""Don't require less than 0 objects or more than 255 objects.""")
+			Arf2Prototype._wgo_required = __idx
+			__R.__cnt = 1
+		elif __R.__cnt == 1:
+			__idx = int(idx)
+			if __idx < 0 or __idx > 255: raise ValueError("""Don't require less than 0 objects or more than 255 objects.""")
+			Arf2Prototype._hgo_required = __idx
+			__R.__cnt = 2
 		return self
-REQUIRE = _R()
+REQUIRE = __R()
+"""
+After Debugging your Arf2 chart[fumen] in the viewer,
+Play the chart[fumen] entirely, and then Add this line at the end of your *.py file:
+	REQUIRE[w][h]
+Param "w" & "h" are provided in the title of the viewer window.
+"""
 
 
+# Environmental Configs
 def OFFSET(ms:int) -> None:
-	Arf2Prototype._offset = ms
+	'''
+	Set the offset(the ms time of the Bar 0) of the Arf2 chart[fumen].
+
+	Args:
+		ms (int): the ms time of the Bar 0.
+
+	Returns:
+		None
+	'''
+	Arf2Prototype._offset = int(ms)
 
 def BEATS_PER_MINUTE(*args) -> None:
-	pass
+	'''
+	Provide the BPM info in 4/4 beats-per-minute form.
+	Recommended for 4/4-time tracks.
+
+	Example:
+		BEATS_PER_MINUTE(
+			0,	200,   # in Bar 0~50, the tempo of the song is 200(in 4/4 beats-per-minute form).
+			50,	190    # since Bar 50, the tempo of the song is 190(in 4/4 beats-per-minute form).
+
+	Args:
+		*args (float): Pass BarPosition & Tempo(in 4/4 beats-per-minute form) alternately.
+
+	Returns:
+		None
+	'''
+	__len = len(args)
+	if __len == 0  or  __len % 2 == 1 :
+		raise BPMInvalidError("Pass BarPosition & Tempo(in 4/4 beats-per-minute form) alternately.")
+	for i in range(0, __len, 2):
+		__bar = float(args[i])
+		__bpm = float(args[i+1]) / 4
+		if __bar < 0:
+			raise ValueError("BarTime cannot be smaller than 0.")
+		elif __bar > 0 and i == 0:
+			raise ValueError("The 1st BarTime must be 0.")
+		if __bpm <= 0:
+			raise ValueError("BPM must be larger than 0.")
+		Arf2Prototype._bpms.append( (__bar,__bpm) )
+	Arf2Prototype._bpms.sort(key = lambda bpmtpl: bpmtpl[0])
 
 def BARS_PER_MINUTE(*args) -> None:
-	pass
+	'''
+	Provide the BPM info in bars-per-minute form.
+	Recommended for non-4/4-time tracks.
+
+	Example:
+		BEATS_PER_MINUTE(
+			0,	50,   # in Bar 0~50, the tempo of the song is 50(in bars-per-minute form).
+			50,	47.5   # since Bar 50, the tempo of the song is 47.5(in bars-per-minute form).
+
+	Args:
+		*args (float): Pass BarPosition & Tempo(in bars-per-minute form) alternately.
+
+	Returns:
+		None
+	'''
+	__len = len(args)
+	if __len == 0  or  __len % 2 == 1 :
+		raise BPMInvalidError("Pass BarPosition & Tempo(in bars-per-minute form) alternately.")
+	for i in range(0, __len, 2):
+		__bar = float(args[i])
+		__bpm = float(args[i+1])
+		if __bar < 0:
+			raise ValueError("BarTime cannot be smaller than 0.")
+		elif __bar > 0 and i == 0:
+			raise ValueError("The 1st BarTime must be 0.")
+		if __bpm <= 0:
+			raise ValueError("BPM must be larger than 0.")
+		Arf2Prototype._bpms.append( (__bar,__bpm) )
+	Arf2Prototype._bpms.sort(key = lambda bpmtpl: bpmtpl[0])
 
 def SC_LAYER1(*args) -> None:
-	pass
+	'''
+	Provide the Speed Scale info of the Arf2 chart[fumen].
+	To be applied to WishChilds in the layer 1.
+
+	Example:
+		SC_LAYER1(
+			0,	1,   # in Bar 0~50, the Speed Scale of layer 1 is 1.
+			50,	1.05   # since Bar 50, the Speed Scale of layer 1 is 1.05 .
+
+	Args:
+		*args (float): Pass BarPosition & SpeedScale alternately.
+
+	Returns:
+		None
+	'''
+	__len = len(args)
+	if __len == 0  or  __len % 2 == 1 :
+		raise ScaleInvalidError("Pass BarPosition & Scale alternately.")
+	for i in range(0, __len, 2):
+		__bar = float(args[i])
+		__scl = float(args[i+1])
+		if __bar < 0:
+			raise ValueError("BarTime cannot be smaller than 0.")
+		elif __bar > 0 and i == 0:
+			raise ValueError("The 1st BarTime must be 0.")
+		if __scl <= 0 and i == 0:
+			raise ValueError("The 1st SpeedScale must be larger than 0.")
+		if Arf2Prototype._sc1_dflt:
+			Arf2Prototype._scales_layer1.pop()
+			Arf2Prototype._sc1_dflt = False
+		Arf2Prototype._scales_layer1.append( (__bar,__scl) )
+	Arf2Prototype._scales_layer1.sort(key = lambda sctpl: sctpl[0])
 
 def SC_LAYER2(*args) -> None:
-	pass
+	'''
+	Provide the Speed Scale info of the Arf2 chart[fumen].
+	To be applied to WishChilds in the layer 2.
+
+	Example:
+		SC_LAYER1(
+			0,	1,   # in Bar 0~50, the Speed Scale of layer 2 is 1.
+			50,	1.05   # since Bar 50, the Speed Scale of layer 2 is 1.05 .
+
+	Args:
+		*args (float): Pass BarPosition & SpeedScale alternately.
+
+	Returns:
+		None
+	'''
+	__len = len(args)
+	if __len == 0  or  __len % 2 == 1 :
+		raise ScaleInvalidError("Pass BarPosition & Scale alternately.")
+	for i in range(0, __len, 2):
+		__bar = float(args[i])
+		__scl = float(args[i+1])
+		if __bar < 0:
+			raise ValueError("BarTime cannot be smaller than 0.")
+		elif __bar > 0 and i == 0:
+			raise ValueError("The 1st BarTime must be 0.")
+		if __scl <= 0 and i == 0:
+			raise ValueError("The 1st SpeedScale must be larger than 0.")
+		if Arf2Prototype._sc2_dflt:
+			Arf2Prototype._scales_layer2.pop()
+			Arf2Prototype._sc2_dflt = False
+		Arf2Prototype._scales_layer2.append( (__bar,__scl) )
+	Arf2Prototype._scales_layer2.sort(key = lambda sctpl: sctpl[0])
 
 
-# Under Construction
+# Composational Functions
 def CURRENT_ANGLE(degree:int) -> None:
-	if degree<-1800 or degree>1800:
+	'''
+	Set the default angle degree value for WishChilds followed.
+
+	Example:
+		CURRENT_ANGLE(90)
+		some_wish.c(some_bartime).c(some_bartime)···   # Angle degree of these 2 WishChilds will be 90
+		CURRENT_ANGLE(0)
+		some_wish.c(some_bartime)···   # Angle degree of the newly-created WishChild will be 0
+
+	Args:
+		degree (int): The angle degree value.
+
+	Returns:
+		None
+	'''
+	degree = int(degree)
+	if degree < -1800  or  degree > 1800:
 		raise ValueError("Degree of AngleNode out of Range [-1800,1800].")
 	Arf2Prototype._current_angle = degree
 
 def specialize_last_hint() -> None:
+	'''
+	Tag the last Hint as a Special Hint.
+	Each Arf2 chart[fumen] contains 0 or 1 Special Hint for some special usages.
+
+	Example:
+		some_wish.               c(some_bartime)
+		specialize_last_hint()   ^  The Hint referring this WishChild won't be the Special Hint, due to the shadowing below.
+		some_wish.               c(some_bartime)
+		specialize_last_hint()   ^  The Hint referring this WishChild will be the Special Hint.
+
+	Args:
+		None
+
+	Returns:
+		None
+	'''
 	Arf2Prototype._last_hint.is_special = True
 
 def w(of_layer2:bool = False, max_visible_distance:float = 7) -> WishGroup:
+	'''
+	Write an Empty Wish into the Arf2 chart[fumen].
+	You may add PosNodes & WishChilds in a method-chaining form.
+
+	Example:
+		w().n(some_bartime,x=1,y=1).c(some_bartime)
+
+	Args:
+		of_layer2 (bool): Is this WishGroup belongs to layer 2?  By default, newly-created WishGroup belongs to layer 1.
+		max_visible_distance (float): The max visible distance for WishChilds, Range [0,8].
+
+	Returns:
+		new_wish (WishGroup): The newly-created WishGroup.
+	'''
+	max_visible_distance = float(max_visible_distance)
+	if max_visible_distance < 0  or  max_visible_distance > 8:
+		raise ValueError("MaxVisibleDistance out of Range [0,8).")
+	elif max_visible_distance > 7.9990234375:
+		max_visible_distance = 7.9990234375
 	_w = WishGroup(of_layer2, max_visible_distance)
 	Arf2Prototype._wish.append(_w)
+	return _w
+
+def wn1(bar:float, nmr:int=0, dnm:int=1, x:float=0, y:float=0, easetype:int=0, curve_init:float=0, curve_end:float=1, max_visible_distance:float = 7) -> WishGroup:
+	'''
+	A shorthand to create a Wish in layer 1 and then attach a PosNode with given arguments.
+
+	Example:
+		wn1(some_bartime,x=1,y=1)
+
+	Args:
+		bar (float): Bartime integer or the Original Bartime
+		nmr (int): numerator to specify the internal position in a bar. Example 5/16 -> 5
+		dnm (int): denominator to specify the internal position in a bar. Example 5/16 -> 16
+		x (float): The x-axis position of the PosNode. Range[-16,32]
+		y (float): The y-axis position of the PosNode. Range[-8,16]
+		easetype (int): EaseType of the AngleNode. Use Pn.* Series.
+		curve_init (float): The initial ratio of the easing curve. Range[0,1]
+		curve_end (float): The end ratio of the easing curve. Range[0,1]
+		max_visible_distance (float): The max visible distance for WishChilds.
+
+	Returns:
+		new_wish (WishGroup): The newly-created WishGroup.
+	'''
+	return w(False, max_visible_distance).n(bar, nmr, dnm, x, y, easetype, curve_init, curve_end)
+
+def wn2(bar:float, nmr:int=0, dnm:int=1, x:float=0, y:float=0, easetype:int=0, curve_init:float=0, curve_end:float=1, max_visible_distance:float = 7) -> WishGroup:
+	'''
+	A shorthand to create a Wish in layer 2 and then attach a PosNode with given arguments.
+
+	Example:
+		wn2(some_bartime,x=1,y=1)
+
+	Args:
+		bar (float): Bartime integer or the Original Bartime
+		nmr (int): numerator to specify the internal position in a bar. Example 5/16 -> 5
+		dnm (int): denominator to specify the internal position in a bar. Example 5/16 -> 16
+		x (float): The x-axis position of the PosNode. Range[-16,32]
+		y (float): The y-axis position of the PosNode. Range[-8,16]
+		easetype (int): EaseType of the AngleNode. Use Pn.* Series.
+		curve_init (float): The initial ratio of the easing curve. Range[0,1]
+		curve_end (float): The end ratio of the easing curve. Range[0,1]
+		max_visible_distance (float): The max visible distance for WishChilds.
+
+	Returns:
+		new_wish (WishGroup): The newly-created WishGroup.
+	'''
+	return w(True, max_visible_distance).n(bar, nmr, dnm, x, y, easetype, curve_init, curve_end)
+
+
+# Official Patterns
