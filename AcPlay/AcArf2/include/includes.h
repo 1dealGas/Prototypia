3d248b3717f8f9c52d82cf39e1ceb2388c721f2a
Fux <109365408+Fuxfantx@users.noreply.github.com>
2024-02-27 22:58:56 +0800

    20240227  libArf2 Header Update

--- AcPlay/AcArf2/include/includes.h
+++ AcPlay/AcArf2/include/includes.h
@@ -4,7 +4,6 @@
 
 
 // Sys Includes
-#include <vector>
 #include <dmsdk/sdk.h>
 #include <dmsdk/dlib/vmath.h>
 #include <dmsdk/dlib/buffer.h>
@@ -13,37 +12,29 @@
 #include <unordered_map>
 
 // Aerials Includes
-#include "arf2_generated.h"
-#include "ease_constants.h"
+#include <arf2_generated.h>   // std::vector included
+#include <ease_constants.h>
 
 
-// Common Structs & Enums
-struct pdp {   // 2-float Structure
-	float p;
-	float dp;
-};
-
-enum {   // Hint Judge Constants
+// Commons
+struct pdp { float p,dp; };   // 2-float Structure
+enum {
+	// Hint Judge Constants
 	HINT_NONJUDGED_NONLIT = 0, HINT_NONJUDGED_LIT,
-	HINT_JUDGED = 10, HINT_JUDGED_LIT, HINT_SWEEPED, HINT_AUTO
-};
+	HINT_JUDGED = 10, HINT_JUDGED_LIT, HINT_SWEEPED, HINT_AUTO,
 
-enum {   // Table Update Constants
+	// Table Update Constants
 	T_WGO = 2, T_HGO, T_AGO_L, T_AGO_R,
 	T_WTINT, T_HTINT, T_ATINT,
-	T_TOUCHES = 4
+	T_TOUCHES = 4,
+
+	// Table Acquire Constants
+	I_MS = 1, E_UPD = 9, E_JUD = 5
 };
 
-enum {   // Table Acquire Constants
-	I_MS = 1,
-	E_UPD = 9,
-	E_JUD = 5
-};
+/* A hack method to get the elem count of a new[] array */
+#define COUNT(x) *( (size_t*)x - 1 )
 
-
-// A hack method to get the elem count of a new[] array
-// Type requirement: Inherited from classes having a non-trivial destructor.
-template <typename T>
-static inline size_t EC(T* addr_returned_by_array_new) {
-	return *( (size_t*)addr_returned_by_array_new - 1 );
-}
+/* Customized Syntax to State Classes with Non-Trivial Destructors */
+#define Node(x) struct x {~x(){}
+#define End };
