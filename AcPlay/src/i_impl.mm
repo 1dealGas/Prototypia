c73dd2332e8a78bf393624d6b5703a8d9f3cb779
Fantx Fux <109365408+Fuxfantx@users.noreply.github.com>
2024-03-18 23:21:21 +0800

    iOS input system done

--- AcPlay/src/i_impl.mm
+++ AcPlay/src/i_impl.mm
@@ -1,14 +1,10 @@
 // Aerials Input System Implementation for iOS
-// #ifdef DM_PLATFORM_IOS
+#ifdef DM_PLATFORM_IOS
 #include <includes.h>
-
-/* C++ Structures */
-// This is mainly a C++ module, and only use Objective-C syntaxes when interacting with iOS APIs.
-struct { double dx = 0.0, dy = 0.0; uint8_t phase = 0; } ArTouches[10], ArtCaches[10];
-uint8_t BCount, MCount, ECount, ACount;   // A: Abandon
-
-
-/* Taptic Engine Stuff */
+struct {
+	double x = 0.0, y = 0.0;
+	uint8_t phase = 2;   // Pressed:0, OnScreen:1, Released:2
+} ArTouches[10];
 
 
 /* A custom UIView to receive input events */
@@ -17,108 +13,155 @@ uint8_t BCount, MCount, ECount, ACount;   // A: Abandon
 @implementation ArInput
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-	// UITouch *touch = [touches anyObject];
-	// CGPoint location = [touch locationInView:self];
-	// NSLog(@"Touch began at location: %@", NSStringFromCGPoint(location));
-
-	/* Since UIResponsor calls the listener on every finger/thumb,
-	 * We use a counter to filter redundant event calls,
-	 * and iterate the whole "event.touches" list.
-	 */
-	if(BCount == 0) {
-		/* Process touches, set Params */
-		ab vf[10];
-
-		/* Do Aerials Calls */
-		const jud jud_result = JudgeArf(vf, BCount, true, false);
-		const bool has_hint_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
-		if(has_hint_judged && haptic_enabled) {
-
-		}
-		
-		// Lua Function I(gui_x, gui_y, gui_phase, play_hitsound)
-		if(EngineLuaState) {
-			lua_getglobal(EngineLuaState, "I");
-			lua_pushnumber(EngineLuaState, 0);
-			lua_pushnumber(EngineLuaState, 0);
-			lua_pushnumber(EngineLuaState, 0);
-			lua_pushboolean(EngineLuaState, has_hint_judged && hitsound_enabled);
-			lua_pcall(EngineLuaState, 4, 0);
-		}
-		
-
-		/* Update Caches */
-		for(uint8_t i=0; i<10; i++) {
-			ArtCaches[i].dx = ArTouches[i].dx;
-			ArtCaches[i].dy = ArTouches[i].dy;
-			ArtCaches[i].phase = ArTouches[i].phase;
-		}
-		BCount--;
+	/* Process touches */
+	for(UITouch *touch in touches) {
+		CGPoint location = [touch locationInView:self.view];
+		NSUInteger id = touch.touchIdentifier % 10;
+		ArTouches[id].x = 900.0 + (location.x - CenterX) / PosDiv;
+		ArTouches[id].y = 540.0 + (3*CenterY - location.y) / PosDiv;   // Remind (0,0) is at the left top of the UIView
+		ArTouches[id].phase = 0;
 	}
-	else
-		BCount--;
+
+	/* Judge & Do Haptics */
+	ab vf[10];
+	uint8_t vfcount;
+	for(uint8_t i=0; i<10; i++)
+		if(ArTouches[i].phase < 2) {
+			vf[vfcount].a = ArTouches[i].x;
+			vf[vfcount].b = ArTouches[i].y;
+			vfcount++;
+		}
+	const jud jud_result = JudgeArf(vf, vfcount, true, false);
+	const bool has_obj_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
+	if(has_obj_judged && haptic_enabled) {
+		UIImpactFeedbackGenerator *haptic_player = [[UIImpactFeedbackGenerator alloc] init];
+		if([haptic_player prepare])
+			[haptic_player impactOccurredWithStyle:UIImpactFeedbackStyleMedium];
+	}
+
+	/* Do Lua Call: function I(gui_x, gui_y, gui_phase, play_hitsound) */
+	if(EngineLuaState) {
+		lua_getglobal(EngineLuaState, "I");
+		lua_pushnumber(EngineLuaState, ArTouches[0].x);
+		lua_pushnumber(EngineLuaState, ArTouches[0].y);
+		lua_pushnumber(EngineLuaState, ArTouches[0].phase);
+		lua_pushboolean(EngineLuaState, has_obj_judged && hitsound_enabled);
+		lua_call(EngineLuaState, 4, 0);
+	}
 }
 
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-	// UITouch *touch = [touches anyObject];
-	// CGPoint location = [touch locationInView:self];
-	// NSLog(@"Touch moved to location: %@", NSStringFromCGPoint(location));
-
-	if(MCount == 0) {
-		/* Process touches, set Params */
-
-		/* Do Aerials Calls */
-
-		/* Update Caches */
-		for(uint8_t i=0; i<10; i++) {
-			ArtCaches[i].dx = ArTouches[i].dx;
-			ArtCaches[i].dy = ArTouches[i].dy;
-			ArtCaches[i].phase = ArTouches[i].phase;
-		}
-		MCount--;
+	/* Process touches */
+	for(UITouch *touch in touches) {
+		CGPoint location = [touch locationInView:self.view];
+		NSUInteger id = touch.touchIdentifier % 10;
+		ArTouches[id].x = 900.0 + (location.x - CenterX) / PosDiv;
+		ArTouches[id].y = 540.0 + (3*CenterY - location.y) / PosDiv;   // Remind (0,0) is at the left top of the UIView
+		ArTouches[id].phase = 1;
 	}
-	else
-		MCount--;
+
+	/* Judge & Do Haptics */
+	ab vf[10];
+	uint8_t vfcount;
+	for(uint8_t i=0; i<10; i++)
+		if(ArTouches[i].phase < 2) {
+			vf[vfcount].a = ArTouches[i].x;
+			vf[vfcount].b = ArTouches[i].y;
+			vfcount++;
+		}
+	const jud jud_result = JudgeArf(vf, vfcount, false, false);
+	const bool has_obj_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
+	if(has_obj_judged && haptic_enabled) {
+		UIImpactFeedbackGenerator *haptic_player = [[UIImpactFeedbackGenerator alloc] init];
+		if([haptic_player prepare])
+			[haptic_player impactOccurredWithStyle:UIImpactFeedbackStyleMedium];
+	}
+
+	/* Do Lua Call: function I(gui_x, gui_y, gui_phase, play_hitsound) */
+	if(EngineLuaState) {
+		lua_getglobal(EngineLuaState, "I");
+		lua_pushnumber(EngineLuaState, ArTouches[0].x);
+		lua_pushnumber(EngineLuaState, ArTouches[0].y);
+		lua_pushnumber(EngineLuaState, ArTouches[0].phase);
+		lua_pushboolean(EngineLuaState, has_obj_judged && hitsound_enabled);
+		lua_call(EngineLuaState, 4, 0);
+	}
 }
 
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-	NSLog(@"Touch ended");
-
-	if(ECount == 0) {
-		/* Process touches, set Params */
-
-		/* Do Aerials Calls */
-
-		/* Update Caches */
-		for(uint8_t i=0; i<10; i++) {
-			ArtCaches[i].dx = ArTouches[i].dx;
-			ArtCaches[i].dy = ArTouches[i].dy;
-			ArtCaches[i].phase = ArTouches[i].phase;
-		}
-		ECount--;
+	/* Process touches */
+	for(UITouch *touch in touches) {
+		CGPoint location = [touch locationInView:self.view];
+		NSUInteger id = touch.touchIdentifier % 10;
+		ArTouches[id].x = 900.0 + (location.x - CenterX) / PosDiv;
+		ArTouches[id].y = 540.0 + (3*CenterY - location.y) / PosDiv;   // Remind (0,0) is at the left top of the UIView
+		ArTouches[id].phase = 2;
 	}
-	else
-		ECount--;
+
+	/* Judge & Do Haptics */
+	ab vf[10];
+	uint8_t vfcount;
+	for(uint8_t i=0; i<10; i++)
+		if(ArTouches[i].phase < 2) {
+			vf[vfcount].a = ArTouches[i].x;
+			vf[vfcount].b = ArTouches[i].y;
+			vfcount++;
+		}
+	const jud jud_result = JudgeArf(vf, vfcount, false, true);
+	const bool has_obj_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
+	if(has_obj_judged && haptic_enabled) {
+		UIImpactFeedbackGenerator *haptic_player = [[UIImpactFeedbackGenerator alloc] init];
+		if([haptic_player prepare])
+			[haptic_player impactOccurredWithStyle:UIImpactFeedbackStyleMedium];
+	}
+
+	/* Do Lua Call: function I(gui_x, gui_y, gui_phase, play_hitsound) */
+	if(EngineLuaState) {
+		lua_getglobal(EngineLuaState, "I");
+		lua_pushnumber(EngineLuaState, ArTouches[0].x);
+		lua_pushnumber(EngineLuaState, ArTouches[0].y);
+		lua_pushnumber(EngineLuaState, ArTouches[0].phase);
+		lua_pushboolean(EngineLuaState, has_obj_judged && hitsound_enabled);
+		lua_call(EngineLuaState, 4, 0);
+	}
 }
 
 - (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-	NSLog(@"Touch cancelled");
-
-	if(ACount == 0) {
-		/* Process touches, set Params */
-
-		/* Do Aerials Calls */
-
-		/* Update Caches */
-		for(uint8_t i=0; i<10; i++) {
-			ArtCaches[i].dx = ArTouches[i].dx;
-			ArtCaches[i].dy = ArTouches[i].dy;
-			ArtCaches[i].phase = ArTouches[i].phase;
-		}
-		ACount--;
+	/* Process touches */
+	for(UITouch *touch in touches) {
+		CGPoint location = [touch locationInView:self.view];
+		NSUInteger id = touch.touchIdentifier % 10;
+		ArTouches[id].x = 900.0 + (location.x - CenterX) / PosDiv;
+		ArTouches[id].y = 540.0 + (3*CenterY - location.y) / PosDiv;   // Remind (0,0) is at the left top of the UIView
+		ArTouches[id].phase = 2;
 	}
-	else
-		ACount--;
+
+	/* Judge & Do Haptics */
+	ab vf[10];
+	uint8_t vfcount;
+	for(uint8_t i=0; i<10; i++)
+		if(ArTouches[i].phase < 2) {
+			vf[vfcount].a = ArTouches[i].x;
+			vf[vfcount].b = ArTouches[i].y;
+			vfcount++;
+		}
+	const jud jud_result = JudgeArf(vf, vfcount, false, true);
+	const bool has_obj_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
+	if(has_obj_judged && haptic_enabled) {
+		UIImpactFeedbackGenerator *haptic_player = [[UIImpactFeedbackGenerator alloc] init];
+		if([haptic_player prepare])
+			[haptic_player impactOccurredWithStyle:UIImpactFeedbackStyleMedium];
+	}
+
+	/* Do Lua Call: function I(gui_x, gui_y, gui_phase, play_hitsound) */
+	if(EngineLuaState) {
+		lua_getglobal(EngineLuaState, "I");
+		lua_pushnumber(EngineLuaState, ArTouches[0].x);
+		lua_pushnumber(EngineLuaState, ArTouches[0].y);
+		lua_pushnumber(EngineLuaState, ArTouches[0].phase);
+		lua_pushboolean(EngineLuaState, has_obj_judged && hitsound_enabled);
+		lua_call(EngineLuaState, 4, 0);
+	}
 }
 
 @end
@@ -138,6 +181,4 @@ uint8_t BCount, MCount, ECount, ACount;   // A: Abandon
 }
 
 @end
-
-
-// #endif
+#endif
