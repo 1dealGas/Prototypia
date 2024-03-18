dc5865b2b01571022d292ea5ea633271a919a8c1
Fantx Fux <109365408+Fuxfantx@users.noreply.github.com>
2024-03-18 18:24:04 +0800

    Add files via upload

--- AcPlay/src/i_impl.mm
+++ AcPlay/src/i_impl.mm
@@ -1,23 +1,57 @@
 // Aerials Input System Implementation for iOS
 #ifdef DM_PLATFORM_IOS
-#import "ViewController.h"
 #include <includes.h>
 
 
 // A custom UIView to receive input events
+#import <UIKit/UIKit.h>
+@interface ArInput : UIView @end
+@implementation ArInput
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:self];
+    NSLog(@"Touch began at location: %@", NSStringFromCGPoint(location));
+
+    // ···
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:self];
+    NSLog(@"Touch moved to location: %@", NSStringFromCGPoint(location));
+
+    // ···
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"Touch ended");
+
+    // ···
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"Touch cancelled");
+
+    // ···
+}
+
+@end
 
 
 // Register the custom UIView into ViewController
-@interface ViewController
-@property(nonatomic, strong) UIView *ArInput;
-@end
-
+#import "ViewController.h"
+@interface ViewController () @end
 @implementation ViewController
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.ArInput = [[UIView alloc] initWithFrame: [[UIScreen mainScreen] bounds] ];
-    [self.view addSubview:self.ArInput];
+    ArInput *AIview = [ [ArInput alloc] initWithFrame: self.view.bounds ];
+    AIview.userInteractionEnabled = YES;
+    [self.view addSubview:AIview];
 }
+
 @end
+
 
 #endif
