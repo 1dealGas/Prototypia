5ffcc95b5c410e43cdff418f4dda0f8b830b33b4
Fantx Fux <109365408+Fuxfantx@users.noreply.github.com>
2024-03-19 09:42:08 +0800

    iOS Input: switch to unordered_map

--- AcPlay/src/i_impl.mm
+++ AcPlay/src/i_impl.mm
@@ -1,10 +1,16 @@
 // Aerials Input System Implementation for iOS
 #ifdef DM_PLATFORM_IOS
 #include <includes.h>
-struct {
+
+
+/* C++ Data Structure */
+struct ArTouch {
 	double x = 0.0, y = 0.0;
 	uint8_t phase = 2;   // Pressed:0, OnScreen:1, Released:2
-} ArTouches[10];
+};
+std::unordered_map<void*, ArTouch> ArTouches;
+ArTouch FirstTouch;
+void* FtId = NULL;
 
 
 /* Create a custom UIView to receive input events */
@@ -15,36 +21,48 @@ struct {
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
 	/* Process touches */
 	for(UITouch *touch in touches) {
-		CGPoint location = [touch locationInView:self.view];
-		NSUInteger id = touch.touchIdentifier % 10;
-		ArTouches[id].x = 900.0 + (location.x - CenterX) / PosDiv;
-		ArTouches[id].y = 540.0 + (3*CenterY - location.y) / PosDiv;   // Remind (0,0) is at the left top of the UIView
-		ArTouches[id].phase = 0;
+		ArTouch new_touch = {
+			900.0 + (location.x - CenterX) / PosDiv,
+			540.0 + (3*CenterY - location.y) / PosDiv,
+			0
+		};
+
+		void* id = (__bridge void*)touch;
+		if( ArTouch.empty() ) {
+			FirstTouch = new_touch;
+			FtId = id;
+		}
+		ArTouch.emplace(id, new_touch);
 	}
 
 	/* Judge & Do Haptics */
-	ab vf[10];
-	uint8_t vfcount;
-	for(uint8_t i=0; i<10; i++)
-		if(ArTouches[i].phase < 2) {
-			vf[vfcount].a = ArTouches[i].x;
-			vf[vfcount].b = ArTouches[i].y;
-			vfcount++;
+	jud jud_result;
+	bool has_obj_judged = false;
+	if(ArfBefore) {
+		ab vf[10];
+		uint8_t vfcount = 0;
+		for(const auto& it : ArTouches) {
+			if(it.second.phase < 2) {
+				vf[vfcount].a = it.second.x;
+				vf[vfcount].b = it.second.y;
+				vfcount++;
+			}
 		}
-	const jud jud_result = JudgeArf(vf, vfcount, true, false);
-	const bool has_obj_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
-	if(has_obj_judged && haptic_enabled) {
-		UIImpactFeedbackGenerator *haptic_player = [[UIImpactFeedbackGenerator alloc] init];
-		if([haptic_player prepare])
-			[haptic_player impactOccurredWithStyle:UIImpactFeedbackStyleMedium];
+		jud_result = JudgeArf(vf, vfcount, true, false);
+		has_obj_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
+		if(has_obj_judged && haptic_enabled) {
+			UIImpactFeedbackGenerator *haptic_player = [[UIImpactFeedbackGenerator alloc] init];
+			if([haptic_player prepare])
+				[haptic_player impactOccurredWithStyle:UIImpactFeedbackStyleMedium];
+		}
 	}
 
 	/* Do Lua Call: function I(gui_x, gui_y, gui_phase, special_hint_judged, play_hitsound) */
 	if(EngineLuaState) {
 		lua_getglobal(EngineLuaState, "I");
-		lua_pushnumber(EngineLuaState, ArTouches[0].x);
-		lua_pushnumber(EngineLuaState, ArTouches[0].y);
-		lua_pushnumber(EngineLuaState, ArTouches[0].phase);
+		lua_pushnumber(EngineLuaState, FtId ? FirstTouch.x : 0.0);
+		lua_pushnumber(EngineLuaState, FtId ? FirstTouch.y : 0.0);
+		lua_pushnumber(EngineLuaState, FtId ? FirstTouch.phase : 2);
 		lua_pushboolean(EngineLuaState, jud_result.special_hint_judged);
 		lua_pushboolean(EngineLuaState, has_obj_judged && hitsound_enabled);
 		lua_call(EngineLuaState, 5, 0);
@@ -54,36 +72,41 @@ struct {
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
 	/* Process touches */
 	for(UITouch *touch in touches) {
+		void* id = (__bridge void*)touch;
 		CGPoint location = [touch locationInView:self.view];
-		NSUInteger id = touch.touchIdentifier % 10;
 		ArTouches[id].x = 900.0 + (location.x - CenterX) / PosDiv;
-		ArTouches[id].y = 540.0 + (3*CenterY - location.y) / PosDiv;   // Remind (0,0) is at the left top of the UIView
+		ArTouches[id].y = 540.0 + (3*CenterY - location.y) / PosDiv;
 		ArTouches[id].phase = 1;
 	}
 
 	/* Judge & Do Haptics */
-	ab vf[10];
-	uint8_t vfcount;
-	for(uint8_t i=0; i<10; i++)
-		if(ArTouches[i].phase < 2) {
-			vf[vfcount].a = ArTouches[i].x;
-			vf[vfcount].b = ArTouches[i].y;
-			vfcount++;
+	jud jud_result;
+	bool has_obj_judged = false;
+	if(ArfBefore) {
+		ab vf[10];
+		uint8_t vfcount = 0;
+		for(const auto& it : ArTouches) {
+			if(it.second.phase < 2) {
+				vf[vfcount].a = it.second.x;
+				vf[vfcount].b = it.second.y;
+				vfcount++;
+			}
 		}
-	const jud jud_result = JudgeArf(vf, vfcount, false, false);
-	const bool has_obj_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
-	if(has_obj_judged && haptic_enabled) {
-		UIImpactFeedbackGenerator *haptic_player = [[UIImpactFeedbackGenerator alloc] init];
-		if([haptic_player prepare])
-			[haptic_player impactOccurredWithStyle:UIImpactFeedbackStyleMedium];
+		jud_result = JudgeArf(vf, vfcount, false, false);
+		has_obj_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
+		if(has_obj_judged && haptic_enabled) {
+			UIImpactFeedbackGenerator *haptic_player = [[UIImpactFeedbackGenerator alloc] init];
+			if([haptic_player prepare])
+				[haptic_player impactOccurredWithStyle:UIImpactFeedbackStyleMedium];
+		}
 	}
 
 	/* Do Lua Call: function I(gui_x, gui_y, gui_phase, special_hint_judged, play_hitsound) */
 	if(EngineLuaState) {
 		lua_getglobal(EngineLuaState, "I");
-		lua_pushnumber(EngineLuaState, ArTouches[0].x);
-		lua_pushnumber(EngineLuaState, ArTouches[0].y);
-		lua_pushnumber(EngineLuaState, ArTouches[0].phase);
+		lua_pushnumber(EngineLuaState, FtId ? FirstTouch.x : 0.0);
+		lua_pushnumber(EngineLuaState, FtId ? FirstTouch.y : 0.0);
+		lua_pushnumber(EngineLuaState, FtId ? FirstTouch.phase : 2);
 		lua_pushboolean(EngineLuaState, jud_result.special_hint_judged);
 		lua_pushboolean(EngineLuaState, has_obj_judged && hitsound_enabled);
 		lua_call(EngineLuaState, 5, 0);
@@ -93,36 +116,41 @@ struct {
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
 	/* Process touches */
 	for(UITouch *touch in touches) {
-		CGPoint location = [touch locationInView:self.view];
-		NSUInteger id = touch.touchIdentifier % 10;
-		ArTouches[id].x = 900.0 + (location.x - CenterX) / PosDiv;
-		ArTouches[id].y = 540.0 + (3*CenterY - location.y) / PosDiv;   // Remind (0,0) is at the left top of the UIView
-		ArTouches[id].phase = 2;
+		ArTouch.erase( (__bridge void*)touch );
+	}
+	if( ArTouch.empty() ) {
+		FirstTouch = {0.0, 0.0, 2};
+		FtId = NULL;
 	}
 
 	/* Judge & Do Haptics */
-	ab vf[10];
-	uint8_t vfcount;
-	for(uint8_t i=0; i<10; i++)
-		if(ArTouches[i].phase < 2) {
-			vf[vfcount].a = ArTouches[i].x;
-			vf[vfcount].b = ArTouches[i].y;
-			vfcount++;
+	jud jud_result;
+	bool has_obj_judged = false;
+	if(ArfBefore) {
+		ab vf[10];
+		uint8_t vfcount = 0;
+		for(const auto& it : ArTouches) {
+			if(it.second.phase < 2) {
+				vf[vfcount].a = it.second.x;
+				vf[vfcount].b = it.second.y;
+				vfcount++;
+			}
 		}
-	const jud jud_result = JudgeArf(vf, vfcount, false, true);
-	const bool has_obj_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
-	if(has_obj_judged && haptic_enabled) {
-		UIImpactFeedbackGenerator *haptic_player = [[UIImpactFeedbackGenerator alloc] init];
-		if([haptic_player prepare])
-			[haptic_player impactOccurredWithStyle:UIImpactFeedbackStyleMedium];
+		jud_result = JudgeArf(vf, vfcount, false, true);
+		has_obj_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
+		if(has_obj_judged && haptic_enabled) {
+			UIImpactFeedbackGenerator *haptic_player = [[UIImpactFeedbackGenerator alloc] init];
+			if([haptic_player prepare])
+				[haptic_player impactOccurredWithStyle:UIImpactFeedbackStyleMedium];
+		}
 	}
 
 	/* Do Lua Call: function I(gui_x, gui_y, gui_phase, special_hint_judged, play_hitsound) */
 	if(EngineLuaState) {
 		lua_getglobal(EngineLuaState, "I");
-		lua_pushnumber(EngineLuaState, ArTouches[0].x);
-		lua_pushnumber(EngineLuaState, ArTouches[0].y);
-		lua_pushnumber(EngineLuaState, ArTouches[0].phase);
+		lua_pushnumber(EngineLuaState, FtId ? FirstTouch.x : 0.0);
+		lua_pushnumber(EngineLuaState, FtId ? FirstTouch.y : 0.0);
+		lua_pushnumber(EngineLuaState, FtId ? FirstTouch.phase : 2);
 		lua_pushboolean(EngineLuaState, jud_result.special_hint_judged);
 		lua_pushboolean(EngineLuaState, has_obj_judged && hitsound_enabled);
 		lua_call(EngineLuaState, 5, 0);
@@ -132,36 +160,41 @@ struct {
 - (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
 	/* Process touches */
 	for(UITouch *touch in touches) {
-		CGPoint location = [touch locationInView:self.view];
-		NSUInteger id = touch.touchIdentifier % 10;
-		ArTouches[id].x = 900.0 + (location.x - CenterX) / PosDiv;
-		ArTouches[id].y = 540.0 + (3*CenterY - location.y) / PosDiv;   // Remind (0,0) is at the left top of the UIView
-		ArTouches[id].phase = 2;
+		ArTouch.erase( (__bridge void*)touch );
+	}
+	if( ArTouch.empty() ) {
+		FirstTouch = {0.0, 0.0, 2};
+		FtId = NULL;
 	}
 
 	/* Judge & Do Haptics */
-	ab vf[10];
-	uint8_t vfcount;
-	for(uint8_t i=0; i<10; i++)
-		if(ArTouches[i].phase < 2) {
-			vf[vfcount].a = ArTouches[i].x;
-			vf[vfcount].b = ArTouches[i].y;
-			vfcount++;
+	jud jud_result;
+	bool has_obj_judged = false;
+	if(ArfBefore) {
+		ab vf[10];
+		uint8_t vfcount = 0;
+		for(const auto& it : ArTouches) {
+			if(it.second.phase < 2) {
+				vf[vfcount].a = it.second.x;
+				vf[vfcount].b = it.second.y;
+				vfcount++;
+			}
 		}
-	const jud jud_result = JudgeArf(vf, vfcount, false, true);
-	const bool has_obj_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
-	if(has_obj_judged && haptic_enabled) {
-		UIImpactFeedbackGenerator *haptic_player = [[UIImpactFeedbackGenerator alloc] init];
-		if([haptic_player prepare])
-			[haptic_player impactOccurredWithStyle:UIImpactFeedbackStyleMedium];
+		jud_result = JudgeArf(vf, vfcount, false, true);
+		has_obj_judged = (bool)(jud_result.early+jud_result.hit+jud_result.late);
+		if(has_obj_judged && haptic_enabled) {
+			UIImpactFeedbackGenerator *haptic_player = [[UIImpactFeedbackGenerator alloc] init];
+			if([haptic_player prepare])
+				[haptic_player impactOccurredWithStyle:UIImpactFeedbackStyleMedium];
+		}
 	}
 
 	/* Do Lua Call: function I(gui_x, gui_y, gui_phase, special_hint_judged, play_hitsound) */
 	if(EngineLuaState) {
 		lua_getglobal(EngineLuaState, "I");
-		lua_pushnumber(EngineLuaState, ArTouches[0].x);
-		lua_pushnumber(EngineLuaState, ArTouches[0].y);
-		lua_pushnumber(EngineLuaState, ArTouches[0].phase);
+		lua_pushnumber(EngineLuaState, FtId ? FirstTouch.x : 0.0);
+		lua_pushnumber(EngineLuaState, FtId ? FirstTouch.y : 0.0);
+		lua_pushnumber(EngineLuaState, FtId ? FirstTouch.phase : 2);
 		lua_pushboolean(EngineLuaState, jud_result.special_hint_judged);
 		lua_pushboolean(EngineLuaState, has_obj_judged && hitsound_enabled);
 		lua_call(EngineLuaState, 5, 0);
@@ -172,31 +205,48 @@ struct {
 
 
 /* Create a custom UIViewController that applies our custom UIView */
-@interface ArInputViewController : UIViewController
-@property(strong, nonatomic) ArInputView *v;
-@end
-
+@interface ArInputViewController : UIViewController @end
 @implementation ArInputViewController
 
 - (void)loadView {
-	self.v = [[ArInputView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-	self.v.backgroundColor = [UIColor clearColor];
-	self.v.userInteractionEnabled = YES;
-	self.view = self.v;
+	ArInputView *v = [[ArInputView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	v.backgroundColor = [UIColor clearColor];
+	v.userInteractionEnabled = YES;
+	self.view = v;
+}
+
+- (void)viewDidLayoutSubviews {  
+    [super viewDidLayoutSubviews];
+	double WindowWthenDiv = self.view.window.bounds.size.width;
+	double WindowHthenDiv = self.view.window.bounds.size.height;
+	CenterX = WindowWthenDiv * 0.5;		CenterY = WindowHthenDiv * 0.5;
+	WindowWthenDiv /= 1800.0;			WindowHthenDiv /= 1080.0;
+	PosDiv = (WindowWthenDiv < WindowHthenDiv) ? WindowWthenDiv : WindowHthenDiv;
 }
 
 @end
 
 
-/* Create a delegate class that applies our custom UIViewController */
-@interface ArDelegate : UIResponder <UIApplicationDelegate>
+/* Create a custom UIWindow class to ban KeyWindow related */
+@interface ArInputWindow : UIWindow @end
+@implementation ArInputWindow
+
+- (BOOL)canBecomeKeyWindow {
+	return NO;
+}
+
+@end
+
+
+/* Create a Delegate class that applies our custom UIViewController */
+@interface ArInputDelegate : UIResponder <UIApplicationDelegate>
 @property(strong, nonatomic) UIWindow *w;
 @end
 
-@implementation ArDelegate
+@implementation ArInputDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	self.w = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	self.w = [[ArInputWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 	self.w.rootViewController = [[ArInputViewController alloc] init];
 	self.w.backgroundColor = [UIColor clearColor];
 	self.w.windowLevel = UIWindowLevelAlert;
@@ -208,14 +258,13 @@ struct {
 
 
 /* Defold Binding */
-ArDelegate* D = NULL;
+ArInputDelegate* D = NULL;
 void InputInit() {
-	D = [[ArDelegate alloc] init];
+	D = [[ArInputDelegate alloc] init];
 	dmExtension::RegisteriOSUIApplicationDelegate(D);
 }
 void InputUninit() {
 	dmExtension::UnregisteriOSUIApplicationDelegate(D);
 	D = NULL;
 }
-
 #endif
