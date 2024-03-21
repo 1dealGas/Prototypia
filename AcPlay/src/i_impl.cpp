dfad32342d7eed58474f2f4308b18b9714de6fe7
Fantx Fux <109365408+Fuxfantx@users.noreply.github.com>
2024-03-21 23:07:27 +0800

    Android Input: Window Size Listening

--- AcPlay/src/i_impl.cpp
+++ AcPlay/src/i_impl.cpp
@@ -12,15 +12,24 @@ double FtX, FtY;		uint8_t FtPhase;
 
 
 /* Window Size Listener */
-#include <android/window.h>
-static void ArNativeWindowResized(ANativeActivity* na, ANativeWindow* w) {
+#include <android/native_window.h>
+static void(*OriginalListener)(ANativeActivity*, ANativeWindow*) = nullptr;
+static void ArNativeWindowResized(ANativeActivity* A, ANativeWindow* W) {
+	double WindowWthenDiv = ANativeWindow_getWidth(W);
+	double WindowHthenDiv = ANativeWindow_getHeight(W);
 
+	CenterX = WindowWthenDiv * 0.5;		CenterY = WindowHthenDiv * 0.5;
+	WindowWthenDiv /= 1800.0;			WindowHthenDiv /= 1080.0;
+	PosDnm = 1.0 / ( (WindowWthenDiv < WindowHthenDiv) ? WindowWthenDiv : WindowHthenDiv );
+
+	if(OriginalListener)   // The "Defensive Programming"
+		OriginalListener(A, W);
 }
 
 
 /* Input Handler */
 #include <android/input.h>
-static int32_t ArInputEvent(struct android_app* app, AInputEvent* event) {
+static int32_t ArInputHandle(struct android_app* A, AInputEvent* E) {
 	return 0;
 }
 
@@ -28,9 +37,18 @@ static int32_t ArInputEvent(struct android_app* app, AInputEvent* event) {
 /* Defold Binding */
 void InputInit() {
 	auto APP = (struct android_app*)dmGraphics::GetNativeAndroidApp();
-	APP->activity->callbacks->onNativeWindowResized = ArNativeWindowResized;
-	APP->onInputEvent = ArInputEvent;
+	const ARect& WindowRect = APP -> contentRect;
+
+	double WindowWthenDiv = WindowRect.right;
+	double WindowHthenDiv = WindowRect.bottom;
+	CenterX = WindowWthenDiv * 0.5;		CenterY = WindowHthenDiv * 0.5;
+	WindowWthenDiv /= 1800.0;			WindowHthenDiv /= 1080.0;
+	PosDnm = 1.0 / ( (WindowWthenDiv < WindowHthenDiv) ? WindowWthenDiv : WindowHthenDiv );
+
+	OriginalListener = APP -> activity -> callbacks -> onNativeWindowResized;
+	APP -> activity -> callbacks -> onNativeWindowResized = ArNativeWindowResized;
+	APP -> onInputEvent = ArInputHandle;
 }
-void InputUninit() {}
+void InputUninit(){}
 
 #endif
